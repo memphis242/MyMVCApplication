c98e40c04e8d60d45dd613e83c7a29826871a255
memphis242 <abdullah.almosalami@ndsu.edu>
2018-12-11 22:08:27 -0600

    Done with Routes section

--- MyMVCApplication/App_Start/RouteConfig.cs
+++ MyMVCApplication/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace MyMVCApplication
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Student",
+                url: "students/{id}/{name}/{standardId}",
+                defaults: new {controller = "Student", action = "Index", id = UrlParameter.Optional, name = UrlParameter.Optional, standardId = UrlParameter.Optional },
+                constraints: new { id = @"\d+" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
