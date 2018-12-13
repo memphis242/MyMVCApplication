231d69d70fb737e8c3876704a6460f49968a4221
memphis242 <abdullah.almosalami@ndsu.edu>
2018-12-12 18:49:53 -0600

    Done with Action methods and first Model section (created Student model).

--- MyMVCApplication/Controllers/StudentController.cs
+++ MyMVCApplication/Controllers/StudentController.cs
@@ -9,9 +9,75 @@ namespace MyMVCApplication.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is Index action method of StudentController";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            // update student to the database
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            // delete student from database whose ID matches specified id
+
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("find")]
+        public ActionResult GetByID(int id)
+        {
+            //get student from database
+
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult PutAction()
+        {
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteAction()
+        {
+            return View("Index");
+        }
+
+        [HttpHead]
+        public ActionResult HeadAction()
+        {
+            return View("Index");
+        }
+
+        [HttpOptions]
+        public ActionResult OptionsAction()
+        {
+            return View("Index");
+        }
+
+        [HttpPatch]
+        public ActionResult PatchAction()
+        {
+            return View("Index");
+        }
+
+        [AcceptVerbs(HttpVerbs.Post | HttpVerbs.Get)]
+        public ActionResult GetAndPostAction()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [NonAction]
+        public Student GetStudent(int id)
+        {
+            return studentList.Where(s => s.StudentID == id).FirstOrDefault();
         }
     }
 }
---
+++ MyMVCApplication/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApplication.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
